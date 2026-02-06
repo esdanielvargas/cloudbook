da821592b930973776c27798923c282aab6d43ad
esdanielvargas <hello@danielvargas.dev>
2026-02-06 00:30:56 -0600

    refactor: enhance Tab and FormField base components

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -12,26 +12,28 @@ export default function Tab({
   path = false,
   href = false,
   action = false,
+  flag = "",
 }) {
+
   if (path) {
     return (
       <Link
         to={`${path}`}
-        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 bg-neutral-100/75 hover:bg-neutral-200/75 dark:bg-neutral-900/75 dark:hover:bg-neutral-800/75 transition-all duration-300 ease-out cursor-pointer"
+        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 hover:bg-neutral-200/50 dark:hover:bg-neutral-800/50 transition-all duration-300 ease-out cursor-pointer"
       >
         {Icon && (
           <div className="size-6 aspect-square flex items-center justify-center">
-            <Icon size={24} strokeWidth={1.5} className="text-neutral-800 dark:text-neutral-200" />
+            <Icon size={24} strokeWidth={1.5} className={`text-neutral-800 dark:text-neutral-200 ${flag === "info" && "text-sky-500!"} ${flag === "danger" && "text-rose-600!"} ${flag === "warning" && "text-red-500!"}`} /> 
           </div>
         )}
         <div className="w-full flex flex-col items-start justify-center">
           {title && (
-            <div className="w-full flex items-center justify-start text-sm md:text-md text-neutral-800 dark:text-neutral-200">
+            <div className={`w-full flex items-center justify-start text-sm md:text-md text-neutral-800 dark:text-neutral-200 ${flag === "info" && "text-sky-500!"} ${flag === "danger" && "text-rose-600!"} ${flag === "warning" && "text-red-500!"}`}>
               {title}
             </div>
           )}
           {caption && (
-            <div className="w-full flex items-center justify-start text-xs md:text-sm text-neutral-600 dark:text-neutral-400">
+            <div className={`w-full flex items-center justify-start text-xs md:text-sm text-neutral-600 dark:text-neutral-400 ${flag === "info" && "text-sky-500/75!"} ${flag === "danger" && "text-rose-600/75!"} ${flag === "warning" && "text-red-500/75!"}`}>
               {caption}
             </div>
           )}
@@ -49,7 +51,7 @@ export default function Tab({
       <a
         href={`${href}`}
         target="_blank"
-        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 -rounded-md bg-neutral-100/75 hover:bg-neutral-200/75 dark:bg-neutral-900/75 dark:hover:bg-neutral-800/75 transition-all duration-300 ease-out cursor-pointer"
+        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 hover:bg-neutral-200/50 dark:hover:bg-neutral-800/50 transition-all duration-300 ease-out cursor-pointer"
       >
         {Icon && (
           <div className="size-6 aspect-square flex items-center justify-center">
@@ -80,7 +82,7 @@ export default function Tab({
     return (
       <button
         type="button"
-        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 -rounded-md bg-neutral-100/75 hover:bg-neutral-200/75 dark:bg-neutral-900/75 dark:hover:bg-neutral-800/75 transition-all duration-300 ease-out cursor-pointer"
+        className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 hover:bg-neutral-200/50 dark:hover:bg-neutral-800/50 transition-all duration-300 ease-out cursor-pointer"
         onClick={() => action()}
       >
         {Icon && (
@@ -110,7 +112,7 @@ export default function Tab({
   }
 
   return (
-    <div className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 -rounded-md bg-neutral-100/75 hover:bg-neutral-200/75 dark:bg-neutral-900/75 dark:hover:bg-neutral-800/75 transition-all duration-300 ease-out cursor-default">
+    <div className="w-full min-h-14 py-3 px-4 flex items-center justify-start gap-4 hover:bg-neutral-200/50 dark:hover:bg-neutral-800/50 transition-all duration-300 ease-out cursor-default">
       {Icon && (
         <div className="size-6 aspect-square flex items-center justify-center">
           <Icon size={24} strokeWidth={1.5} className="text-neutral-800 dark:text-neutral-200" />
