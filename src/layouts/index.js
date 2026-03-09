6ff8302cf358ba34f4fd3f4fc52aae42741e2793
esdanielvargas <hello@danielvargas.dev>
2026-03-09 01:35:35 -0600

    refactor(layouts): remove unused imports and exports from main index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,4 @@
 import AuthLayout from "./AuthLayout.jsx";
 import AppLayout from "./AppLayout.jsx";
-import HomeLayout from "./HomeLayout.jsx";
-import ProfileLayout from "./ProfileLayout.jsx";
-import PreferencesLayout from "./PreferencesLayout.jsx";
 
-export { AuthLayout, AppLayout, HomeLayout, ProfileLayout, PreferencesLayout };
+export { AuthLayout, AppLayout };
